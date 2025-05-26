f7be7aeeddd9a19e88e7fde708df1efe89b40a91
binhminh276 <23110266@student.hcmute.edu.vn>
2025-05-26 23:18:52 +0700

    pull

--- Program.cs
+++ Program.cs
@@ -19,8 +19,8 @@ namespace CompanyHRManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new LoginForm());
-            Application.Run(new MainForm_user("chuongminh@gmail.com"));
+            Application.Run(new LoginForm());
+            //Application.Run(new MainForm_user("chuongminh@gmail.com"));
             //Application.Run(new MainForm_admin("chuong@gmail.com"));
         }
     }
